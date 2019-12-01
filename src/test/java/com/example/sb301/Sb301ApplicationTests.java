a5930d7cb1da8df9f18dee2056762f5d3bad793a
Eathyn Edwards <eathynedwards@Earyns-MacBook-Air.local>
2019-12-01 16:09:50 -0500

    Initial commit

---
+++ src/test/java/com/example/sb301/Sb301ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sb301;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb301ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
